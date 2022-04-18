902337afe609e7b9fc03894a358df9036f0838c4
jeeltpatel <patel.jeel@northeastern.edu>
2022-04-18 14:53:51 -0400

    :heavy_plus_sign: Added Insurance Company Enterprise

---
+++ src/ChemoCare/Enterprise/InsuranceCompanyEnterprise.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ChemoCare.Enterprise;
+
+import ChemoCare.Org.Org;
+import ChemoCare.Role.Role;
+import ChemoCare.Insurance.InsurancePlanDirectory;
+import ChemoCare.InsuranceCustomer.InsuranceCustomerDirectory;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author jeelpatel
+ */
+public class InsuranceCompanyEnterprise extends Enterprise {
+       
+    private InsuranceCustomerDirectory insuranceCustomerDirectory;
+
+    private InsurancePlanDirectory insurancePlanDirectory;
+    
+      public InsuranceCompanyEnterprise(String orgName, int zip, String address) {
+        super(orgName, EnterpriseType.InsuranceCompany, zip,address);
+        insuranceCustomerDirectory = new InsuranceCustomerDirectory();
+        insurancePlanDirectory = new InsurancePlanDirectory();
+
+    }
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+
+    @Override
+    public List<Org.Type> getAllOrganizationTypes() {
+        List<Org.Type> orgTypes = new ArrayList<>();
+        orgTypes.add(Org.Type.InsuranceAgent);
+        orgTypes.add(Org.Type.InsurancePolicyPlanner);
+        orgTypes.add(Org.Type.InsuranceFinanceOrganization);
+
+        return orgTypes;
+    }
+
+    public InsuranceCustomerDirectory getInsuranceCustomerDirectory() {
+        return insuranceCustomerDirectory;
+    }
+
+    public void setInsuranceCustomerDirectory(InsuranceCustomerDirectory insuranceCustomerDirectory) {
+        this.insuranceCustomerDirectory = insuranceCustomerDirectory;
+    }
+
+    public InsurancePlanDirectory getInsurancePlanDirectory() {
+        return insurancePlanDirectory;
+    }
+
+    public void setInsurancePolicyDirectory(InsurancePlanDirectory insurancePolicyDirectory) {
+        this.insurancePlanDirectory = insurancePolicyDirectory;
+    }
+
+}
