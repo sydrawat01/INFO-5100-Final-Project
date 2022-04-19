cbbb4f797b32ba72541f42fdfb3a8a3708f2202f
Siddharth Rawat <rawat.sid@northeastern.edu>
2022-04-19 12:49:07 -0400

    :man: Add Manager and Patient roles

--- src/ChemoCare/Role/Patient.java
+++ src/ChemoCare/Role/Patient.java
@@ -1,9 +1,19 @@
 package ChemoCare.Role;
 
+import ChemoCare.Account.Account;
+import ChemoCare.Ecosystem;
+import ChemoCare.Enterprise.Enterprise;
+import ChemoCare.Org.Org;
+import UI.PatientRole.PatientWorkArea;
+import javax.swing.JPanel;
+
 /**
  *
  * @author sid
  */
-public class Patient {
-  
+public class Patient extends Role {
+  @Override
+  public JPanel createWorkArea(JPanel userProcessContainer, Account account, Org org, Enterprise enterprise, Ecosystem business) {
+    return new PatientWorkArea();
+  }
 }
