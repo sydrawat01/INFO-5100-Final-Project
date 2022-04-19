33e27c8b7c546ec4b9b9cf0347ee788450172209
Siddharth Rawat <rawat.sid@northeastern.edu>
2022-04-19 13:18:08 -0400

    :heavy_plus_sign: Add Doctor and Lab Attendant roles with UI skeleton

--- src/ChemoCare/Role/Doctor.java
+++ src/ChemoCare/Role/Doctor.java
@@ -1,9 +1,20 @@
 package ChemoCare.Role;
 
+import ChemoCare.Account.Account;
+import ChemoCare.Ecosystem;
+import ChemoCare.Enterprise.Enterprise;
+import ChemoCare.Org.Org;
+import UI.DoctorRole.DoctorWorkArea;
+import javax.swing.JPanel;
+
 /**
  *
  * @author sid
  */
-public class Doctor {
+public class Doctor extends Role {
+  @Override
+  public JPanel createWorkArea(JPanel userProcessContainer, Account userAccount, Org org, Enterprise enterprise, Ecosystem business) {
+    return new DoctorWorkArea();
+  }
   
 }
--- src/ChemoCare/Role/LabAttendant.java
+++ src/ChemoCare/Role/LabAttendant.java
@@ -1,9 +1,19 @@
 package ChemoCare.Role;
 
+import ChemoCare.Account.Account;
+import ChemoCare.Ecosystem;
+import ChemoCare.Enterprise.Enterprise;
+import ChemoCare.Org.Org;
+import UI.LabAttendant.LabAttendantWorkArea;
+import javax.swing.JPanel;
+
 /**
  *
  * @author sid
  */
-public class LabAttendant {
-  
+public class LabAttendant extends Role {
+  @Override
+  public JPanel createWorkArea(JPanel userProcessContainer, Account account, Org org, Enterprise enterprise, Ecosystem business) {
+    return new LabAttendantWorkArea();
+  }
 }
