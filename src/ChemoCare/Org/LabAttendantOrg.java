fb5466fb0997fc633835a059fb12cc0e90455281
jeeltpatel <patel.jeel@northeastern.edu>
2022-04-19 13:27:26 -0400

    :heavy_plus_sign: Added Lab Attendent

--- src/ChemoCare/Org/LabAttendantOrg.java
+++ src/ChemoCare/Org/LabAttendantOrg.java
@@ -3,11 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ChemoCare.Org;
-
+import ChemoCare.Role.LabAttendant;
+import ChemoCare.Role.Role;
+import java.util.ArrayList;
 /**
  *
  * @author jeelpatel
  */
-public class LabAttendantOrg {
-    
+public class LabAttendantOrg extends Org{
+    public LabAttendantOrg() {
+        super(Org.Type.Lab.getValue());
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new LabAttendant());
+        return roles;
+    }
 }
