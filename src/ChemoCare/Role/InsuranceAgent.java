2c4100cee18328ace451802078e4abad1f86f0d1
Siddharth Rawat <rawat.sid@northeastern.edu>
2022-04-20 16:30:28 -0400

    :office_worker: Add insurance agent annd UI skeleton

--- src/ChemoCare/Role/InsuranceAgent.java
+++ src/ChemoCare/Role/InsuranceAgent.java
@@ -1,9 +1,19 @@
 package ChemoCare.Role;
 
+import ChemoCare.Account.Account;
+import ChemoCare.Ecosystem;
+import ChemoCare.Enterprise.Enterprise;
+import ChemoCare.Org.Org;
+import UI.InsuranceAgent.InsuranceAgentWorkArea;
+import javax.swing.JPanel;
+
 /**
  *
  * @author sid
  */
-public class InsuranceAgent {
-  
+public class InsuranceAgent extends Role {
+  @Override
+  public JPanel createWorkArea(JPanel userProcessContainer, Account account, Org org, Enterprise enterprise, Ecosystem business) {
+    return new InsuranceAgentWorkArea();
+  }
 }
