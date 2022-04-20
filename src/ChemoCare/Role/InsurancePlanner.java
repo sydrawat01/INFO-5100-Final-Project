e00b2173fe18eb6d08a27a7048ee5230eff32143
Siddharth Rawat <rawat.sid@northeastern.edu>
2022-04-20 17:04:27 -0400

    :health_worker: Add insurance planner role and skeleton UI

--- src/ChemoCare/Role/InsurancePlanner.java
+++ src/ChemoCare/Role/InsurancePlanner.java
@@ -1,9 +1,19 @@
 package ChemoCare.Role;
 
+import ChemoCare.Account.Account;
+import ChemoCare.Ecosystem;
+import ChemoCare.Enterprise.Enterprise;
+import ChemoCare.Org.Org;
+import UI.InsurancePlanner.InsurancePlannerWorkArea;
+import javax.swing.JPanel;
+
 /**
  *
  * @author sid
  */
-public class InsurancePlanner {
-  
+public class InsurancePlanner extends Role {
+  @Override
+  public JPanel createWorkArea(JPanel userProcessContainer, Account account, Org org, Enterprise enterprise, Ecosystem business) {
+    return new InsurancePlannerWorkArea();
+  }
 }
