3dc7c2c75c709e5cf7c23da39acbbaa286e55745
Siddharth Rawat <rawat.sid@northeastern.edu>
2022-04-20 16:20:45 -0400

    :information_desk_person: Add Govt Secretary role and UI skeleton

--- src/ChemoCare/Role/GovtSecretary.java
+++ src/ChemoCare/Role/GovtSecretary.java
@@ -1,9 +1,19 @@
 package ChemoCare.Role;
 
+import ChemoCare.Account.Account;
+import ChemoCare.Ecosystem;
+import ChemoCare.Enterprise.Enterprise;
+import ChemoCare.Org.Org;
+import UI.GovtSecretary.SecretaryWorkArea;
+import javax.swing.JPanel;
+
 /**
  *
  * @author sid
  */
-public class GovtSecretary {
-  
+public class GovtSecretary extends Role {
+  @Override
+  public JPanel createWorkArea(JPanel userProcessContainer, Account userAccount, Org org, Enterprise enterprise, Ecosystem business) {
+    return new SecretaryWorkArea();
+  }
 }
