d418c4c434befe23a28dbfd2946685ea12508b60
jeeltpatel <patel.jeel@northeastern.edu>
2022-04-20 17:26:05 -0400

    :heavy_plus_sign: Paitent Organization

--- src/ChemoCare/Org/PatientOrg.java
+++ src/ChemoCare/Org/PatientOrg.java
@@ -4,10 +4,26 @@
  */
 package ChemoCare.Org;
 
+import ChemoCare.Role.GovtHealthOfficial;
+import ChemoCare.Role.Patient;
+import ChemoCare.Role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author jeelpatel
  */
-public class PatientOrg {
+public class PatientOrg extends Org{
+    
+    public PatientOrg() {
+        super(Org.Type.Patient.getValue());
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new Patient());
+        return roles;
+    }
     
 }
