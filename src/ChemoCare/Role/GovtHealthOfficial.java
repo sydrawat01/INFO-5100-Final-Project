06544ed360f81a2ee0cd8d66318acdc4b34b5949
Siddharth Rawat <rawat.sid@northeastern.edu>
2022-04-20 16:10:05 -0400

    :cop: Add govt. health official role and UI skeleton

--- src/ChemoCare/Role/GovtHealthOfficial.java
+++ src/ChemoCare/Role/GovtHealthOfficial.java
@@ -1,9 +1,19 @@
 package ChemoCare.Role;
 
+import ChemoCare.Account.Account;
+import ChemoCare.Ecosystem;
+import ChemoCare.Enterprise.Enterprise;
+import ChemoCare.Org.Org;
+import UI.HealthcareOfficial.HealthOfficialWorkArea;
+import javax.swing.JPanel;
+
 /**
  *
  * @author sid
  */
-public class GovtHealthOfficial {
-  
+public class GovtHealthOfficial  extends Role {
+  @Override
+  public JPanel createWorkArea(JPanel userProcessContainer, Account userAccount, Org org, Enterprise enterprise, Ecosystem business) {
+    return new HealthOfficialWorkArea();
+  }
 }
