54c1785d0e873c0b64f14b2a261b238f05f32516
Siddharth Rawat <rawat.sid@northeastern.edu>
2022-04-20 17:21:58 -0400

    :moneybag: Add Insurance Treasurer role with UI skeleton

--- src/ChemoCare/Role/InsuranceTreasurer.java
+++ src/ChemoCare/Role/InsuranceTreasurer.java
@@ -1,9 +1,19 @@
 package ChemoCare.Role;
 
+import ChemoCare.Account.Account;
+import ChemoCare.Ecosystem;
+import ChemoCare.Enterprise.Enterprise;
+import ChemoCare.Org.Org;
+import UI.InsuranceTreasurer.InsuranceTreasurerWorkArea;
+import javax.swing.JPanel;
+
 /**
  *
  * @author sid
  */
-public class InsuranceTreasurer {
-  
+public class InsuranceTreasurer extends Role {
+  @Override
+  public JPanel createWorkArea(JPanel userProcessContainer, Account account, Org org, Enterprise enterprise, Ecosystem business) {
+    return new InsuranceTreasurerWorkArea();
+  }
 }
