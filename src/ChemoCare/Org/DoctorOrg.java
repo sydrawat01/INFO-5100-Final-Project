9e72d0fa3729e45a214a215a897322091a9d4c72
jeeltpatel <patel.jeel@northeastern.edu>
2022-04-20 15:43:47 -0400

    :heavy_plus_sign: Doctor added

--- src/ChemoCare/Org/DoctorOrg.java
+++ src/ChemoCare/Org/DoctorOrg.java
@@ -4,10 +4,23 @@
  */
 package ChemoCare.Org;
 
+import ChemoCare.Role.Doctor;
+import ChemoCare.Role.Role;
+import java.util.ArrayList;
 /**
  *
  * @author jeelpatel
  */
-public class DoctorOrg {
-    
+public class DoctorOrg extends Org {
+     public DoctorOrg() {
+        super(Org.Type.Doctor.getValue());
+    }
+     
+         
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new Doctor());
+        return roles;
+    }
 }
