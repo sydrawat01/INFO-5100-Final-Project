9c4504f2f380745cc0364a39e21f9bbb992c7b59
jeeltpatel <patel.jeel@northeastern.edu>
2022-04-21 16:17:07 -0400

    :heavy_plus_sign: Secretary Organization Added

--- src/ChemoCare/Org/SecretaryOrg.java
+++ src/ChemoCare/Org/SecretaryOrg.java
@@ -4,10 +4,26 @@
  */
 package ChemoCare.Org;
 
+import ChemoCare.Role.GovtHealthOfficial;
+import ChemoCare.Role.GovtSecretary;
+import ChemoCare.Role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author jeelpatel
  */
-public class SecretaryOrg {
+public class SecretaryOrg extends Org{
+    
+     public SecretaryOrg() {
+        super(Org.Type.Secretary.getValue());
+    }
+     
+      @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new GovtSecretary());
+        return roles;
+    }
     
 }
