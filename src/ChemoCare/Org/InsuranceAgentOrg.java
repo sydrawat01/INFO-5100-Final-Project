f046eab06638504a88fead5a9a67765005ce96d5
jeeltpatel <patel.jeel@northeastern.edu>
2022-04-22 12:14:27 -0400

    :heavy_plus_sign: Insurance Agent Organization Added

--- src/ChemoCare/Org/InsuranceAgentOrg.java
+++ src/ChemoCare/Org/InsuranceAgentOrg.java
@@ -4,10 +4,25 @@
  */
 package ChemoCare.Org;
 
+
+import ChemoCare.Role.GovtHealthOfficial;
+import ChemoCare.Role.InsuranceAgent;
+import ChemoCare.Role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author jeelpatel
  */
-public class InsuranceAgentOrg {
-    
+public class InsuranceAgentOrg extends Org {
+    public InsuranceAgentOrg() {
+        super(Org.Type.InsuranceAgent.getValue());
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new InsuranceAgent());
+        return roles;
+    }
 }
