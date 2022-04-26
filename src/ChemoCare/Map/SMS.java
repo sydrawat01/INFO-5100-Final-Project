e603e5f10a1a3e7cd324a7e96944f822d6a658e9
jeeltpatel <patel.jeel@northeastern.edu>
2022-04-26 11:29:29 -0400

    :mailbox_with_mail: Email,SMS and Google Maps added

--- src/ChemoCare/Map/SMS.java
+++ src/ChemoCare/Map/SMS.java
@@ -3,11 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ChemoCare.Map;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
 /**
  *
  * @author jeelpatel
  */
 public class SMS {
-    
+      public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+
+    public static void SendSMS(String to, String msg){
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber(to),
+                new com.twilio.type.PhoneNumber("+16176524988"),
+                msg)
+            .create();
+
+        System.out.println(message.getSid());
+    }
 }
